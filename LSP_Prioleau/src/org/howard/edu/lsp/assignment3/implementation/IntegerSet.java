8d23dd671109c0d2fc0aa498de9d117eaea0ba8b
Howard Ai <prioleau1123@gmail.com>
2021-09-28 17:37:46 -0400

    v1

--- LSP_Prioleau/src/org/howard/edu/lsp/assignment3/implementation/IntegerSet.java
+++ LSP_Prioleau/src/org/howard/edu/lsp/assignment3/implementation/IntegerSet.java
@@ -1,22 +1,24 @@
 package org.howard.edu.lsp.assignment3.implementation;
 import java.util.ArrayList;
 /**
- * 
+ * Creates a IntergerSet object that is based on a Arraylist
  * @author Howard P
  * @version 1.0
  */
 public class IntegerSet{
 	// Hint: probably best to use an array list.  You will need to do a little research
-	private ArrayList<int> set = new ArrayList<int>();
+	private ArrayList<Integer> set = new ArrayList<Integer>();
 
 	// Default Constructor
 	public void Set() {
 		
 	}
 
-	// Clears the internal representation of the set
+	/**
+	 * Clears the internal representation of the set
+	 */
 	public void clear() {
-		
+		set = new ArrayList<Integer>();
 	}
 	
 	/**
@@ -39,12 +41,9 @@ public class IntegerSet{
 	public boolean equals(IntegerSet b){
 		boolean found = false;
 		for (int i = 0; i < set.size(); i++) {
-			for (int j = 0; i < b.size(); j++) {
-				//IF THE VALUE IS FOUND IN THE LIST it WILL make found true
-				if (set[i] == b[j]) {
-					found = true;
-					break;
-				}
+			if(b.contains(set.get(i))){
+				found = true;
+				break;
 			}
 			//if found it will continue to the next item in the array
 			if (found){
@@ -58,59 +57,183 @@ public class IntegerSet{
 		//Since it was able to run through without breaking means the sets are equal
 		return true;
 	}
-	// Returns true if the set contains the value, otherwise false
+
+	/**
+	 * Returns true if the set contains the value, otherwise false
+	 * 
+	 * @param value is a int object that you want to see if the set contains
+	 * @return a boolean if the set has the value given
+	 */
 	public boolean contains(int value){
-		boolean found = false;
 		for (int i = 0; i < set.size(); i++) {
-			if (set[i] == value) {
-				return found;
+			if (set.get(i) == value) {
+				return true;
 			}
-			return found;
+		}
+		return false;
+	}
+	
+	/**
+	 * Returns the largest item in the set 
+	 * Throws a IntegerSetException if the set is empty 
+	 * 
+	 * @return the largest value in the set
+	 * @throws IntegerSetException if the set is empty
+	 */
+	public int largest() throws IntegerSetException{
+		if (set.size() == 0) {
+			throw new IntegerSetException("The IntegerSet is empty");
+		}
+		int largVal = set.get(0);
+		for (int i = 0; i < set.size(); i++) {
+			if (set.get(i) > largVal) {
+				largVal = set.get(i);
+			}
+		}
+		return largVal;
+	}
+	
+	/**
+	 *Returns the smallest item in the set
+	 *Throws a IntegerSetException if the set is empty
+	 *
+	 * @return the smallest item in the set
+	 * @throws IntegerSetException if the set is empty
+	 */
+	public int smallest() throws IntegerSetException {
+		if (set.size() == 0) {
+			throw new IntegerSetException("The IntegerSet is empty");
+		}
+		int smallVal = set.get(0);
+		for (int i = 0; i < set.size(); i++) {
+			if (set.get(i) < smallVal) {
+				smallVal = set.get(i);
+			}
+		}
+		return smallVal;
+	}
+	
+	/**
+	 * Adds an item to the set or does nothing it already there
+	 * 
+	 * 
+	 * @param item is a int object that you want added to the set
+	 */
+ 	public void add(int item) {
+ 		if(!(set.contains(item))){
+ 			set.add(item);
+		}
+ 	}
+
+
+ 	/**
+ 	 * Removes an item from the set or does nothing if not there
+ 	 * Throws a IntegerSetException if the set is empty
+ 	 * 
+ 	 * @param item is a int object that you want removed to the set
+ 	 * @throws IntegerSetException if the set is empty
+ 	 */
+	public void remove(int item) throws IntegerSetException{
+		//
+		if (set.size() == 0) {
+			throw new IntegerSetException("The IntegerSet is empty");
+		}
+		if(set.contains(item)){
+			for (int i = 0; i < set.size(); i++) {
+				if (set.get(i) == item) {
+					set.remove(i);
+				}
+			}
 		}
 	}
 	
-	// Returns the largest item in the set; Throws a IntegerSetException if the set is empty 
-	public int largest(){
-		//throws IntegerSetException
-	}
-	
-	// Returns the smallest item in the set; Throws a IntegerSetException if the set is empty
-	public int smallest() {
-		//throws IntegerSetException;
-	}
-	
-	// Adds an item to the set or does nothing it already there	
- 	public void add(int item) {
- 		// adds item to s or does nothing if it is in set
- 	}
-
-	// Removes an item from the set or does nothing if not there
-	public void remove(int item) {
-		//Throws a IntegerSetException of the set is empty
-	}
-	
-	// Set union
+	/**
+	 * Set union
+	 * Combines 2 integerSets
+	 * 
+	 * @param intSetb is a integerSet object
+	 */
 	public void union(IntegerSet intSetb){
-		
+		set.addAll(intSetb.getArrayList());
 	}
 	
-	// Set intersection
+
+	/**
+	 * Creates a new set of the intersection of both objects
+	 * 
+	 * @param intSetb is a IntergerSet object you want to be intersected with the set
+	 */
 	public void intersect(IntegerSet intSetb) {
-		
+		ArrayList<Integer> set2 = new ArrayList<Integer>();
+		for (int i = 0; i < set.size(); i++) {
+			if(intSetb.contains(set.get(i))){
+				set2.add(set.get(i));
+			}
+		}
+		set = set2;
 	}
 	
-	// Set difference, i.e., s1 –s2
+	/**
+	 * Creates a new set from the difference of both objects
+	 * 
+	 * @param intSetb is a IntergerSet object you want to be difference with the set
+	 */
 	public void diff(IntegerSet intSetb){
-		// set difference, i.e. s1 - s2
+		ArrayList<Integer> set2 = new ArrayList<Integer>();
+		for (int i = 0; i < set.size(); i++) {
+			if(!(intSetb.contains(set.get(i)))){
+				set2.add(set.get(i));
+			}
+		}
+		set = set2;
 	}
 	
-	// Returns true if the set is empty, false otherwise
+	/**
+	 * Returns the ArrayList of the IntergerSet Object
+	 * 
+	 * @return the ArrayList of the IntergerSet Object
+	 */
+	public ArrayList getArrayList() {
+		return set;
+	}
+
+	/**
+	 * Returns true if the set is empty, false otherwise
+	 * 
+	 * @return boolean whether the set is empty or not
+	 */
 	boolean isEmpty() {
-		
+		if (set.size() == 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 	
-	// Return String representation of your set
+
+	/**
+	 * Creates a String representation of your set
+	 * 
+	 * @return The String Representation of my set
+	 */
 	public String toString(){
 		// return String representation of your set
-	}	
+		String setString = " ";
+		for (int i = 0; i < set.size() - 1 ; i++) {
+			setString += String.valueOf(set.get(i)) + " , ";
+		}
+		setString += String.valueOf(set.get((set.size() - 1)));
+		return setString;
+		
 	}
+
+}
+
+public class IntegerSetException extends Exception {  
+	private static final long serialVersionUID = 7035980445904148530L;
+	public IntegerSetException() {}
+	public IntegerSetException(String errorMessage) {  
+		super(errorMessage);  
+    }  
+}  
--- LSP_Prioleau/src/org/howard/edu/lsp/assignment3/test/Driver.java
+++ LSP_Prioleau/src/org/howard/edu/lsp/assignment3/test/Driver.java
@@ -1,4 +1,6 @@
 package org.howard.edu.lsp.assignment3.test;
+import org.howard.edu.lsp.assignment3.implementation.IntegerSet;
+import org.howard.edu.lsp.assignment3.implementation.IntegerSetException;
 /**
  * 
  * @author Howard P
@@ -8,27 +10,28 @@ public class Driver {
 
 	/**
 	 * @param args
+	 * @throws IntegerSetException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IntegerSetException {
 		// TODO Auto-generated method stub
 		IntegerSet set1 = new IntegerSet();
 		set1.add(1);
 		set1.add(2);
 		set1.add(3);
 
-		System.out.println(“Value of Set1 is:” + set1.toString());
-		System.out.println(“Smallest value in Set1 is:” + set1.smallest());
-		System.out.println(“Largest value in Set1 is:” + set1.largest());
+		System.out.println("Value of Set1 is:" + set1.toString());
+		System.out.println("Smallest value in Set1 is:" + set1.smallest());
+		System.out.println("Largest value in Set1 is:" + set1.largest());
 
 		IntegerSet set2 = new IntegerSet();
 		set2.add(4);
 		set2.add(5);
 
-		System.out.println(“Union of Set1 and Set2”);
-		System.out.println(“Value of Set1 is:” + set1.toString());
-		System.out.println(“Value of Set2 is:” + set2.toString());
+		System.out.println("Union of Set1 and Set2");
+		System.out.println("Value of Set1 is:" + set1.toString());
+		System.out.println("Value of Set2 is:" + set2.toString());
 		set1.union(set2);	// union of set1 and set2
-		System.out.println(“Result of union of Set1 and Set2”);
+		System.out.println("Result of union of Set1 and Set2");
 		set1.toString();	// result of union of set1 and set2
 
 
